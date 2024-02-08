d95b701e6a945187eef03a486acf7d35c3ab76ac
minnukota381 <minnukota381@gmail.com>
2024-02-08 20:21:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    double firstNum;
+    String operation;
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button num0 = findViewById(R.id.num0);
+        Button num1 = findViewById(R.id.num1);
+        Button num2 = findViewById(R.id.num2);
+        Button num3 = findViewById(R.id.num3);
+        Button num4 = findViewById(R.id.num4);
+        Button num5 = findViewById(R.id.num5);
+        Button num6 = findViewById(R.id.num6);
+        Button num7 = findViewById(R.id.num7);
+        Button num8 = findViewById(R.id.num8);
+        Button num9 = findViewById(R.id.num9);
+
+        Button ac = findViewById(R.id.ac);
+        Button del = findViewById(R.id.del);
+        Button div = findViewById(R.id.div);
+        Button times = findViewById(R.id.times);
+        Button min = findViewById(R.id.min);
+        Button equal = findViewById(R.id.equal);
+        Button plus = findViewById(R.id.plus);
+        Button point = findViewById(R.id.point);
+        Button percentage = findViewById(R.id.percentage);
+
+        TextView screen = findViewById(R.id.screen);
+
+        ac.setOnClickListener(view -> {
+            firstNum = 0;
+            screen.setText("0");
+        });
+
+        ArrayList<Button> nums = new ArrayList<>();
+        nums.add(num0);
+        nums.add(num1);
+        nums.add(num2);
+        nums.add(num3);
+        nums.add(num4);
+        nums.add(num5);
+        nums.add(num6);
+        nums.add(num7);
+        nums.add(num8);
+        nums.add(num9);
+
+        for (Button b : nums) {
+            b.setOnClickListener(view -> {
+                if (!screen.getText().toString().equals("0")) {
+                    screen.setText(screen.getText().toString() + b.getText().toString());
+                } else {
+                    screen.setText(b.getText().toString());
+                }
+            });
+        }
+
+        ArrayList<Button> opers = new ArrayList<>();
+        opers.add(div);
+        opers.add(times);
+        opers.add(plus);
+        opers.add(min);
+        for (Button b : opers) {
+            b.setOnClickListener(view -> {
+                firstNum = Double.parseDouble(screen.getText().toString());
+                operation = b.getText().toString();
+                screen.setText("");
+            });
+        }
+
+        del.setOnClickListener(view -> {
+            String num = screen.getText().toString();
+            if (num.length() > 1) {
+                screen.setText(num.substring(0, num.length() - 1));
+            } else if (num.length() == 1 && !num.equals("0")) {
+                screen.setText("0");
+            }
+        });
+
+        point.setOnClickListener(view -> {
+            if (!screen.getText().toString().contains(".")) {
+                screen.setText(screen.getText().toString() + ".");
+            }
+        });
+
+        percentage.setOnClickListener(view -> {
+            double num = Double.parseDouble(screen.getText().toString());
+            double result = num / 100;
+            screen.setText(String.valueOf(result));
+        });
+
+        equal.setOnClickListener(view -> {
+            String secondNumText = screen.getText().toString();
+            double secondNum = Double.parseDouble(secondNumText);
+            double result;
+            switch (operation) {
+                case "/":
+                    result = firstNum / secondNum;
+                    break;
+                case "x":
+                    result = firstNum * secondNum;
+                    break;
+                case "+":
+                    result = firstNum + secondNum;
+                    break;
+                case "-":
+                    result = firstNum - secondNum;
+                    break;
+                default:
+                    result = firstNum + secondNum;
+            }
+            screen.setText(String.valueOf(result));
+            firstNum = result;
+        });
+    }
+}
